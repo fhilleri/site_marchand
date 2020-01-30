aa5e13dedbcf54372f910158c6e9a138b77e2ac1
fhilleri <46034382+fhilleri@users.noreply.github.com>
2020-01-31 00:09:21 +0100

    Debut barre de navigation

---
+++ pages/accueil.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Site marchand</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet"> 
+    </head>
+
+    <body>
+        <header>
+            <div id="nav_bar">
+                <img src="../img/menu.png">
+                <img src="../img/logo.png" id="logo">
+                <form>
+                    <input type="text">
+                </form>
+                <div id="separator"></div>
+                <button id="connect">Se connecter</button>
+            </div>
+        </header>
+    </body>
+</html>
